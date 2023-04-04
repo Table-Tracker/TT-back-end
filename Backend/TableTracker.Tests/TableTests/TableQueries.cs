99f44d1212a661b0a0a07a2b2ffeac5867f83829
Максим Шимків <322topgamerintheworld228@gmail.com>
2023-04-05 00:51:43 +0300

    Add user tests...I guess...

---
+++ Backend/TableTracker.Tests/TableTests/TableQueries.cs
@@ -0,0 +1,160 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableTracker.Domain.Interfaces.Repositories;
+using TableTracker.Domain.Interfaces;
+using Xunit;
+using AutoMapper;
+using System.Threading;
+using TableTracker.Application.CQRS.Restaurants.Queries.FindRestaurantById;
+using TableTracker.Domain.DataTransferObjects;
+using TableTracker.Domain.Entities;
+using TableTracker.Infrastructure.Repositories;
+using TableTracker.Application.CQRS.Tables.Queries.FindTableById;
+using TableTracker.Application.CQRS.Franchises.Queries.GetAllFranchises;
+using TableTracker.Application.CQRS.Tables.Queries.GetAllTables;
+using TableTracker.Application.CQRS.Reservations.Queries.GetAllReservations;
+using TableTracker.Application.CQRS.Tables.Queries.GetAllTablesByRestaurant;
+
+namespace TableTracker.Tests.TableTests
+{
+    public class TableQueries
+    {
+        private Mock<IUnitOfWork<long>> unitOfWorkMock;
+        private Mock<ITableRepository> _tableRepository;
+
+        public TableQueries()
+        {
+            unitOfWorkMock = new Mock<IUnitOfWork<long>>();
+            _tableRepository = new Mock<ITableRepository>();
+
+            unitOfWorkMock
+                .Setup(uow => uow.GetRepository<ITableRepository>())
+                .Returns(_tableRepository.Object);
+        }
+
+        [Fact]
+        public async void FindTableById()
+        {
+            var query = new FindTableByIdQuery(1);
+
+            var table = new Table { Id = 1 };
+            var tableDTO = new TableDTO { Id = 1 };
+
+            _tableRepository
+                .Setup(repo => repo.FindById(1))
+                .ReturnsAsync(table);
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock
+                .Setup(mapper => mapper.Map<TableDTO>(table))
+                .Returns(tableDTO);
+
+            var handler = new FindTableByIdQueryHandler(unitOfWorkMock.Object, mapperMock.Object);
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.Equal(table.Id, result.Id);
+        }
+
+        [Fact]
+        public async void GetAlltables()
+        {
+            var tables = new List<Table>()
+            {
+                new Table { Id = 1 },
+                new Table { Id = 2 },
+                new Table { Id = 3 },
+                new Table { Id = 4 },
+                new Table { Id = 5 },
+            };
+
+            var tablesDTO = new List<TableDTO>()
+            {
+                new TableDTO { Id = 1 },
+                new TableDTO { Id = 2 },
+                new TableDTO { Id = 3 },
+                new TableDTO { Id = 4 },
+                new TableDTO { Id = 5 },
+            };
+
+            var query = new GetAllTablesQuery();
+
+            _tableRepository
+                .Setup(repo => repo.GetAll())
+                .ReturnsAsync(tables);
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(mapper => mapper.Map<TableDTO>(It.IsAny<Table>()))
+                .Returns((Table c) => new TableDTO { Id = c.Id });
+
+            var handler = new GetAllTablesQueryHandler(unitOfWorkMock.Object, mapperMock.Object);
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            var check = true;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i].Id != tablesDTO[i].Id)
+                {
+                    check = false;
+                    break;
+                }
+            }
+
+            Assert.True(check);
+        }
+
+        [Fact]
+        public async void GetAllTablesByRestaurant()
+        {
+            var restaurant = new Restaurant { Id = 1, Email = "LMAO@g.com" };
+            var resDto = new RestaurantDTO { Id = 1, Email = "LMAO@g.com" };
+
+            var tables = new List<Table>()
+            {
+                new Table { RestaurantId = 1, Restaurant = restaurant, Id = 1 },
+                new Table { RestaurantId = 1, Restaurant = restaurant, Id = 2 },
+                new Table { Id = 3 },
+                new Table { Id = 4 },
+                new Table { Id = 5 },
+            };
+            var tablesDTO = new List<TableDTO>()
+            {
+                new TableDTO { Restaurant = resDto, Id = 1 },
+                new TableDTO { Restaurant = resDto, Id = 2 },
+            };
+
+            var query = new GetAllTablesByRestaurantQuery(1);
+
+            _tableRepository
+                .Setup(repo => repo.GetAllTablesByRestaurant(1))
+                .ReturnsAsync(new List<Table>(tables.Take(2)));
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(mapper => mapper.Map<TableDTO>(It.IsAny<Table>()))
+                 .Returns((Table c) => new TableDTO
+                 {
+                     Id = c.Id,
+                 });
+
+            var handler = new GetAllTablesByRestaurantQueryHandler(unitOfWorkMock.Object, mapperMock.Object);
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            var check = true;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i].Id != tablesDTO[i].Id)
+                {
+                    check = false;
+                    break;
+                }
+            }
+
+            Assert.True(check);
+        }
+    }
+}
---
+++ Backend/TableTracker.Tests/UserTests/UserQueries.cs
@@ -0,0 +1,178 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableTracker.Domain.Interfaces.Repositories;
+using TableTracker.Domain.Interfaces;
+using Xunit;
+using AutoMapper;
+using System.Threading;
+using TableTracker.Application.CQRS.Tables.Queries.FindTableById;
+using TableTracker.Domain.DataTransferObjects;
+using TableTracker.Infrastructure.Repositories;
+using TableTracker.Application.CQRS.Users.Queries.FindUserById;
+using TableTracker.Domain.Entities;
+using TableTracker.Application.CQRS.Tables.Queries.GetAllTables;
+using TableTracker.Application.CQRS.Users.Queries.GetAllUsers;
+using TableTracker.Application.CQRS.Users.Queries.GetAllUsersByFullName;
+using TableTracker.Application.CQRS.Users.Queries.GetUserByEmail;
+
+namespace TableTracker.Tests.UserTests
+{
+    public class UserQueries
+    {
+        private Mock<IUnitOfWork<long>> unitOfWorkMock;
+        private Mock<IUserRepository> _userRepository;
+
+        public UserQueries()
+        {
+            unitOfWorkMock = new Mock<IUnitOfWork<long>>();
+            _userRepository = new Mock<IUserRepository>();
+
+            unitOfWorkMock
+                .Setup(uow => uow.GetRepository<IUserRepository>())
+                .Returns(_userRepository.Object);
+        }
+
+        [Fact]
+        public async void FindUserById()
+        {
+            var query = new FindUserByIdQuery(1);
+
+            var user = new User { Id = 1 };
+            var userDTO = new UserDTO { Id = 1 };
+
+            _userRepository
+                .Setup(repo => repo.FindById(1))
+                .ReturnsAsync(user);
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock
+                .Setup(mapper => mapper.Map<UserDTO>(user))
+                .Returns(userDTO);
+
+            var handler = new FindUserByIdQueryHandler(unitOfWorkMock.Object, mapperMock.Object);
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.Equal(user.Id, result.Id);
+        }
+
+        [Fact]
+        public async void GetAllUsers()
+        {
+            var users = new List<User>()
+            {
+                new User { Id = 1 },
+                new User { Id = 2 },
+                new User { Id = 3 },
+                new User { Id = 4 },
+                new User { Id = 5 },
+            };
+
+            var usersDTO = new List<UserDTO>()
+            {
+                new UserDTO { Id = 1 },
+                new UserDTO { Id = 2 },
+                new UserDTO { Id = 3 },
+                new UserDTO { Id = 4 },
+                new UserDTO { Id = 5 },
+            };
+
+            var query = new GetAllUsersQuery();
+
+            _userRepository
+                .Setup(repo => repo.GetAll())
+                .ReturnsAsync(users);
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(mapper => mapper.Map<UserDTO>(It.IsAny<User>()))
+                .Returns((User c) => new UserDTO { Id = c.Id });
+
+            var handler = new GetAllUsersQueryHandler(unitOfWorkMock.Object, mapperMock.Object);
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            var check = true;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i].Id != usersDTO[i].Id)
+                {
+                    check = false;
+                    break;
+                }
+            }
+
+            Assert.True(check);
+        }
+
+        [Fact]
+        public async void GetAllUsersByFullName()
+        {
+            var users = new List<User>()
+            {
+                new User { Id = 1, FullName = "John Smith" },
+                new User { Id = 2, FullName = "John Smith"},
+                new User { Id = 3, FullName = "John Smith1"},
+                new User { Id = 4, FullName = "John Smith2"},
+                new User { Id = 5, FullName = "John Smith3"},
+            };
+
+            var usersDTO = new List<UserDTO>()
+            {
+                new UserDTO { Id = 1, FullName = "John Smith" },
+                new UserDTO { Id = 2, FullName = "John Smith"},
+            };
+
+            var query = new GetAllUsersByFullNameQuery("John Smith");
+
+            _userRepository
+                .Setup(repo => repo.GetAllUsersByFullName("John Smith"))
+                .ReturnsAsync(new List<User>(users.Take(2)));
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(mapper => mapper.Map<UserDTO>(It.IsAny<User>()))
+                .Returns((User c) => new UserDTO { Id = c.Id, FullName = c.FullName });
+
+            var handler = new GetAllUsersByFullNameQueryHandler(unitOfWorkMock.Object, mapperMock.Object);
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            var check = true;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i].Id != usersDTO[i].Id || result[i].FullName != usersDTO[i].FullName)
+                {
+                    check = false;
+                    break;
+                }
+            }
+
+            Assert.True(check);
+        }
+
+        [Fact]
+        public async void GetAllUsersByEmail()
+        {
+            var query = new GetUserByEmailQuery("JohnSmith@g.com");
+
+            var user = new User { Id = 1, Email = "JohnSmith@g.com" };
+            var userDTO = new UserDTO { Id = 1, Email = "JohnSmith@g.com" };
+
+            _userRepository
+                .Setup(repo => repo.GetUserByEmail("JohnSmith@g.com"))
+                .ReturnsAsync(user);
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock
+                .Setup(mapper => mapper.Map<UserDTO>(user))
+                .Returns(userDTO);
+
+            var handler = new GetUserByEmailQueryHandler(unitOfWorkMock.Object, mapperMock.Object);
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.Equal(user.Id, result.Id);
+        }
+    }
+}
